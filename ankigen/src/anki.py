b16fc6112f2df4f4cecf25ac9b5596f5e9b3c58d
mdhoffman271 <mdhoffman271@gmail.com>
2024-05-11 19:57:03 +0000

    Add google translate

--- ankigen/src/anki.py
+++ ankigen/src/anki.py
@@ -1,6 +1,7 @@
 
 from typing import Iterable
 
+from ankigen.src.google_translate import get_translation_url
 from ankigen.src.sample import Sample
 
 
@@ -8,15 +9,12 @@ def escape_str(text: str) -> str:
     return f'"{text.replace('"', '""')}"'
 
 
-def save_dict_as_anki(path: str, data: dict[str, str]) -> None:
-    with open(path, 'w', encoding='utf8') as file:
-        for key, value in data.items():
-            file.write(f'{escape_str(key)};{escape_str(value)}\n')
-
-
 def save_samples_as_anki(path: str, samples: Iterable[Sample]) -> None:
     with open(path, 'w', encoding='utf8') as file:
         for sample in samples:
             front = sample.text
-            back = '' if sample.url is None else f'<a href="{sample.url}">Source</a>'
+            back_parts = [f'<a href="{get_translation_url(sample.text, sample.lang)}">Translate</a>']
+            if sample.url is not None:
+                back_parts.append(f'<a href="{sample.url}">Source</a>')
+            back = '<br>'.join(back_parts)
             file.write(f'{escape_str(front)};{escape_str(back)}\n')
---
+++ ankigen/src/google_translate.py
@@ -0,0 +1,5 @@
+from urllib.parse import quote, unquote, urlparse
+
+
+def get_translation_url(text: str, source_lang: str, target_lang: str = 'en') -> str:
+    return f'https://translate.google.com/?sl={source_lang}&tl={target_lang}&text={quote(text)}&op=translate'
