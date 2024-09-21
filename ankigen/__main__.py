331bef814cd6e3004937e07efe21ee483845a258
mdhoffman271 <mdhoffman271@gmail.com>
2024-09-21 19:43:41 +0000

    Fix formatting

--- ankigen/__main__.py
+++ ankigen/__main__.py
@@ -1,16 +1,18 @@
 from ankigen.src.study.anki_bulider import AnkiBuilder
 
+# todo cli
+
 
 def main():
     for lang in ['de', 'es', 'fr']:
-        builder = (
+        (
             AnkiBuilder(lang)
             .with_epub(f'./data/epub/{lang}/**/*.epub')
             .with_kaikki('./data/kaikki/raw-wiktextract-data.jsonl.gz')
             .with_text(f'./data/text/{lang}/**/*.txt')
             .with_firefox_wiktionary('./data/firefox/places.sqlite', 61.0)
+            .generate('./data/out/')
         )
-        builder.generate('./data/out/')
 
 
 main()
