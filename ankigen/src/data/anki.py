151f30734851cbc92e5ddafe52791e2afcb20669
mdhoffman271 <mdhoffman271@gmail.com>
2024-09-23 01:00:10 +0000

    Escape html

--- ankigen/src/data/anki.py
+++ ankigen/src/data/anki.py
@@ -1,4 +1,4 @@
-
+import html
 from typing import Iterable
 
 import validators
@@ -11,18 +11,17 @@ def escape_str(text: str) -> str:
     return f'"{text.replace('"', '""')}"'
 
 
-# todo escape html
 def save_samples_as_anki(path: str, samples: Iterable[Sample], lang: str) -> None:
     with open(path, 'w', encoding='utf8') as file:
         for sample in samples:
-            front = sample.text
+            front = html.escape(sample.text)
             back_parts = [f'<a href="{get_translation_url(sample.text, lang)}">Translate</a>']
             if sample.source is not None:
                 if validators.url(sample.source):
                     back_parts.append(f'<a href="{sample.source}">Source</a>')
                 else:
-                    back_parts.append(sample.source)
+                    back_parts.append(html.escape(sample.source))
             if sample.english is not None:
-                back_parts.append(sample.english)
+                back_parts.append(html.escape(sample.english))
             back = '<br><br>'.join(back_parts)
             file.write(f'{escape_str(front)};{escape_str(back)}\n')
